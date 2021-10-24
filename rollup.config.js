95e5af7e4c2224ee18f24e7e1872ccdbf0876e6b
ecitlm <ecitlm@163.com>
2021-10-24 20:08:11 +0800

    'feat:新增单文件打包'

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import { babel } from '@rollup/plugin-babel';
 import { terser } from 'rollup-plugin-terser';
+import glob from 'glob';
 import pkg from './package.json';
 
 const comments = `/**
@@ -12,37 +13,69 @@ const comments = `/**
 * Date: ${new Date().toLocaleDateString()}
 **/
 `;
-export default {
-  input: './src/index.js',
-  output: [
-    {
-      file: pkg.browser,
-      format: 'umd', // umd是兼容amd/cjs/iife的通用打包格式，适合浏览器
-      name: 'webUtils' //当format为iife和umd时必须提供，将作为全局变量挂在window(浏览器环境)下：window.webUtils
+/**
+ * 插件
+ *
+ * @type {Plugin[]}
+ */
+const plugins = [
+  babel({
+    exclude: 'node_modules/**',
+    babelHelpers: 'runtime'
+  }),
+  resolve(), // 查找和打包node_modules中的第三方模块
+  commonjs(), // 将 CommonJS 转换成 ES2015 模块供 Rollup 处理
+  terser({
+    compress: {
+      pure_funcs: ['console.log']
     },
-    {
-      file: pkg.main,
-      format: 'cjs'
-    },
-    {
-      file: pkg.module,
-      format: 'esm'
+    output: {
+      preamble: comments
     }
-  ],
-  plugins: [
-    babel({
-      exclude: 'node_modules/**',
-      babelHelpers: 'runtime'
-    }),
-    resolve(), // 查找和打包node_modules中的第三方模块
-    commonjs(), // 将 CommonJS 转换成 ES2015 模块供 Rollup 处理
-    terser({
-      compress: {
-        pure_funcs: ['console.log']
-      },
+  })
+];
+/**
+ * 动态JS获取
+ * @param path
+ * @returns {[]}
+ */
+function getEntriesJs(path) {
+  let entryList = [];
+  glob.sync(path).forEach(entry => {
+    let fileName = entry.replace(/(.*\/)*([^.]+).*/gi, '$2');
+    entryList.push({
+      input: entry,
       output: {
-        preamble: comments
+        file: `dist/lib/${fileName}.js`,
+        format: 'umd',
+        name: fileName
+      },
+      plugins
+    });
+  });
+  return entryList;
+}
+const entryJS = getEntriesJs('src/methods/*.js');
+
+export default [
+  {
+    input: './src/index.js',
+    output: [
+      {
+        file: pkg.browser,
+        format: 'umd', // umd是兼容amd/cjs/iife的通用打包格式，适合浏览器
+        name: 'webUtils' // 当format为iife和umd时必须提供，将作为全局变量挂在window(浏览器环境)下：window.webUtils
+      },
+      {
+        file: pkg.main,
+        format: 'cjs'
+      },
+      {
+        file: pkg.module,
+        format: 'esm'
       }
-    })
-  ]
-};
+    ],
+    plugins
+  },
+  ...entryJS
+];
