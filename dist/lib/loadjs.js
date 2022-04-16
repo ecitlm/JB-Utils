afd5b8d8c9d265d68a1d8877ba0a88375964d839
ecitlm <ecitlm@163.com>
2022-04-16 16:38:14 +0800

    update

---
+++ dist/lib/loadjs.js
@@ -0,0 +1,10 @@
+/**
+* name: jb-utils
+* version 1.1.2
+* description: jb-utils前端常用方法库
+* author: ecitlm
+* website: https://ecitlm.github.io/JB-Utils/index.html
+* Date: 4/16/2022
+**/
+
+!function(e,t){"object"==typeof exports&&"undefined"!=typeof module?module.exports=t():"function"==typeof define&&define.amd?define(t):(e="undefined"!=typeof globalThis?globalThis:e||self).loadjs=t()}(this,(function(){"use strict";return function(e){var t=arguments.length>1&&void 0!==arguments[1]?arguments[1]:Function,n=document.createElement("script");n.type="text/javascript",n.readyState?n.onreadystatechange=function(){"loaded"!=n.readyState&&"complete"!=n.readyState||(n.onreadystatechange=null,t())}:n.onload=function(){t()},n.src=e,document.getElementsByTagName("head")[0].appendChild(n)}}));
--- dist/lib/treeToArray.js
+++ dist/lib/treeToArray.js
@@ -1,10 +1,10 @@
 /**
 * name: jb-utils
-* version 1.1.1
+* version 1.1.2
 * description: jb-utils前端常用方法库
 * author: ecitlm
 * website: https://ecitlm.github.io/JB-Utils/index.html
-* Date: 2021-12-21
+* Date: 4/16/2022
 **/
 
 !function(e,r){"object"==typeof exports&&"undefined"!=typeof module?module.exports=r():"function"==typeof define&&define.amd?define(r):(e="undefined"!=typeof globalThis?globalThis:e||self).treeToArray=r()}(this,(function(){"use strict";return function e(r,t){var n=[];if(t=t||"children",!Array.isArray(r))throw new Error("arguments must be array");for(var o=0;o<r.length;o++){var i=r[o],f=JSON.parse(JSON.stringify(i));if(delete f[t],n.push(f),i[t]){var a=e(i[t],t);n=n.concat(a)}}return n}}));
